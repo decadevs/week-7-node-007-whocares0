f34e1ad91182591f50ed6cef541abbebacc3de23
whocares0 <kazeem.adewole@decagon.dev>
2021-05-10 14:05:48 +0100

    finished

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -7,6 +7,9 @@ var express_1 = __importDefault(require("express"));
 var controller_1 = require("../controllers/controller");
 var router = express_1.default.Router();
 /* GET home page. */
+router.get("/", function (req, res) {
+    res.send("<h1>let us calculate the area of the following shapes: square,rectangle,triange,circle</h1>\n\nin the format of {shape: takes a string,dimension: an object for rectangle and triangle or a number for square and circle}\n");
+});
 router.get('/fetchRecords', controller_1.fetchRecords);
 router.post("/calculate", controller_1.calculate);
 exports.default = router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,11 @@ import { calculate, fetchRecords } from '../controllers/controller';
 const router = express.Router();
 
 /* GET home page. */
+router.get("/", (req:Request,res:Response)=> {
+res.send(`<h1>let us calculate the area of the following shapes: square,rectangle,triange,circle</h1>\n
+in the format of {shape: takes a string,dimension: an object for rectangle and triangle or a number for square and circle}
+`)
+})
 router.get('/fetchRecords',fetchRecords);
 router.post("/calculate", calculate)
 
